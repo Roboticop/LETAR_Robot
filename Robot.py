5c88d7bc53711d8a8aeabae2d5b9ad6f5b6f1ebd
Nathaniel Wert <N8.Wert.B@gmail.com>
2021-01-19 17:16:29 +0000

    Created a robot class to contain all of the movement.  Also created a Z and XY Movement class to contain their respective movement functions.

---
+++ Robot.py
@@ -0,0 +1,8 @@
+from movement import XY_Motor_Movement, Z_Motor_Movement
+
+class Robot(XY_Motor_Movement, Z_Motor_Movement):
+
+  def __init__(self, motor_1_pins, motor_2_pins, motor_3_pins):
+    XY_Motor_Movement.__init__(motor_1_pins, motor_2_pins)
+    Z_Motor_Movement.__init__(motor_3_pins)
+  
--- movement.py
+++ movement.py
@@ -1,6 +1,6 @@
 import l293d
 
-class Motors():
+class XY_Motor_Movement():
   '''Motors will set up the robots motors and functions will be called to move the robot'''
   def __init__(self, motor_1_pins, motor_2_pins):
     self.left_motor = l293d.DC(motor_1_pins)
@@ -21,3 +21,18 @@ class Motors():
   def Move_Left(self):
     self.left_motor.anticlockwise()
     self.right_motor.anticlockwise()
+
+  def Stop_moving(self):
+    self.left_motor.stop()
+    self.right_motor.stop()
+
+class Z_Motor_Movement():
+  '''Motor that adjusts the height of the robot'''
+  def __init__(self, motor_3_pins):
+    self.z_motor = l293d.DC(motor_3_pins)
+
+  def Move_Up(self):
+    self.z_motor.clockwise()
+
+  def Move_Down(self):
+    self.z_motor.anticlockwise()
